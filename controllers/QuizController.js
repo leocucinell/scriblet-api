f134d43e84c3bb69533b040f74cc9b582c126845
Leo Cucinell <leocucinell@gmail.com>
2022-03-05 11:52:51 -1000

    added base crud for subject methods

--- controllers/QuizController.js
+++ controllers/QuizController.js
@@ -1,37 +1,69 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 //SECTION: Methods
 
 //BASE URL: /quiz
 //POST: /add
-const addQuiz = (req, res) => {
+const addQuiz = async (req, res) => {
     try{
-        
+        const createdQuiz = await prisma.quiz.create({
+            data: {
+                title: "Test Quiz title",
+                subject_id: 1,
+                student_id: 2
+            }
+        });
+        res.send(createdQuiz);
     } catch(err) {
         console.log(err);
     }
 }
 
 //GET: /:id
-const getQuiz = (req, res) => {
+const getQuiz = async (req, res) => {
     try{
-
+        const retrievedQuiz = await prisma.quiz.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            include: {
+                questions: true
+            }
+        });
+        res.send(retrievedQuiz);
     } catch(err) {
         console.log(err);
     }
 }
 
 //PUT: /:id
-const editQuiz = (req, res) => {
+const editQuiz = async (req, res) => {
     try{
-
+        const editedQuiz = await prisma.quiz.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: {
+                title: 'updated Quiz Title'
+                //last_accessed: 'Update me to include a new datetime object'
+            }
+        });
+        res.send(editedQuiz);
     } catch(err) {
         console.log(err);
     }
 }
 
 //DELETE: /:id
-const deleteQuiz = (req, res) => {
+const deleteQuiz = async (req, res) => {
     try{
-
+        const deletedQuiz = await prisma.quiz.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        res.send(deletedQuiz);
     } catch(err) {
         console.log(err);
     }
--- controllers/SubjectController.js
+++ controllers/SubjectController.js
@@ -1,37 +1,63 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
 //SECTION: Methods
 
 //BASE URL: /student
 //POST: /add
-const addSubject = (req, res) => {
+const addSubject = async (req, res) => {
     try{
-
+        const createdSubject = await prisma.subject.create({
+            data: {
+                title: "Subject TEst title",
+                owner_id: 1
+            }
+        });
+        res.send(createdSubject);
     } catch(err) {
         console.log(err);
     }
 }
 
 //GET: /:id
-const getSubject = (req, res) => {
+const getSubject = async (req, res) => {
     try{
-
+        const retrievedSubject = await prisma.subject.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+        res.send(retrievedSubject);
     } catch(err) {
         console.log(err);
     }
 }
 
 //PUT: /:id
-const editSubject = (req, res) => {
+const editSubject = async (req, res) => {
     try{
-
+        const editedSubject = await prisma.subject.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: {
+                title: 'Edited Subject Title'
+            }
+        });
+        res.send(editedSubject);
     } catch(err) {
         console.log(err);
     }
 }
 
 //DELETE: /:id
-const deleteSubject = (req, res) => {
+const deleteSubject = async (req, res) => {
     try{
-
+        const deletedSubject = await prisma.subject.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
     } catch(err) {
         console.log(err);
     }
